9ba8053765937be9d1da814d65a3f29f0cba4535
FUJI Goro (gfx) <gfuji@cpan.org>
2014-10-05 12:14:29 +0900

    hello RubyMotion!

--- app/main_activity.rb
+++ app/main_activity.rb
@@ -1,5 +1,14 @@
 class MainActivity < Android::App::Activity
   def onCreate(savedInstanceState)
     super
+
+    # R.layout.hello
+    layoutId = resources.getIdentifier('hello', 'layout', packageName)
+    self.contentView = layoutId
+
+    # R.id.text
+    viewId = resources.getIdentifier('text', 'id', packageName)
+    textView = findViewById(viewId)
+    textView.text = "Hello, RubyMotion!"
   end
 end
